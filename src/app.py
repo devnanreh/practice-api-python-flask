d25119afb0d1d820da8e476036b8f1720cf750c7
nanher94 <hernan.albiol@gmail.com>
2022-09-10 17:33:18 +0000

    Practica ToDo List con Python Flask

---
+++ src/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request, json
+
+app = Flask(__name__)
+
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+
+
+
+@app.route('/todos', methods=['GET'])
+def get_tasks():
+    json_text = jsonify(todos)
+    return json_text
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = json.loads(request.data)
+    todos.append(request_body)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
+
+
+
+
+
+# These two lines should always be at the end of your app.py file.
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
